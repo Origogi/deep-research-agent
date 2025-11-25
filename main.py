8fe57f0dccc36b5834fc04981b4b7ba925e109c1
link.origogi(김정태)/kakao <link.origogi@kakaocorp.com>
2025-11-25 23:06:30 +0900

    수정중

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from deep-research-agent!")
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_agentchat.conditions import MaxMessageTermination, TextMentionTermination
+from autogen_agentchat.ui import Console
 
+model = OpenAIChatCompletionClient(model="gpt-4o-mini")
 
-if __name__ == "__main__":
-    main()
+clarity_agent = AssistantAgent(
+    model_client=model,
+    system_message=""
+)
+
+tone_agent = AssistantAgent(
+    model_client=model,
+    system_message=""
+)
+
+persuasion_agent = AssistantAgent(
+    model_client=model,
+    system_message=""
+)
+
+synthesizer_agent = AssistantAgent(
+    model_client=model,
+    system_message=""
+)
+
+critic_agent = AssistantAgent(
+    model_client=model,
+    system_message=""
+)
